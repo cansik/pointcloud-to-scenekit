f6e8b656b1bcd32ac9b06355626bf0068843c9e9
Florian Bruggisser <florian@bildspur.ch>
2019-02-23 13:29:43 +0100

    initial version commit

---
+++ PLYToSceneKit/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  PLYToSceneKit
+//
+//  Created by Florian Bruggisser on 2019-02-23.
+//  Copyright © 2019 bildspur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
